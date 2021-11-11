19678f03e1fd37851eb3ad30cd04fd290d77aa8a
gabrielerut <93905554+gabrielerut@users.noreply.github.com>
2021-11-11 22:29:07 +0200

    My first commit

---
+++ Testavimo kursai 2021/Test/Open24Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testavimo_kursai_2021.Test
+{
+    class Open24Test
+    {
+    }
+}
