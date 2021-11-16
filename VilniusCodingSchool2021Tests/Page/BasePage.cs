db4f59c7a93054ebaf6ec97a062b83467c097601
gabrielerut <93905554+gabrielerut@users.noreply.github.com>
2021-11-16 23:16:29 +0200

    New commit new project

---
+++ VilniusCodingSchool2021Tests/Page/BasePage.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VilniusCodingSchool2021Tests.Page
+{
+    public class BasePage
+    {
+        protected static IWebDriver driver;
+
+        public BasePage(IWebDriver webDriver)
+        {
+            driver = webDriver;
+        }
+
+        public void CloseBrowser()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ VilniusCodingSchool2021Tests/Test/FinalProjectTest.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VilniusCodingSchool2021Tests.Page;
+
+namespace VilniusCodingSchool2021Tests.Test
+{
+    class FinalProjectTest
+    {
+        private static FinalProjectLoginPage _page;
+
+        [OneTimeSetUp]
+        public static void SetUp()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+            _page = new FinalProjectLoginPage(driver);
+            /*_page2 = new Open24Homepage(driver);
+            _page3 = new Open24ChildrenSection(driver);*/
+        }
+        [OneTimeTearDown]
+        public static void TearDown()
+        {
+            //_driver.Quit();
+        }
+        [TestCase("gabriele.rutkauske@gmail.com", "KodasTestui", "Mano užsakymai", TestName = "Test Valid LogIn")]
+        public void TestValidLogIn(string email, string password, string result)
+        {
+            _page.InsertEmailAndPassword(email, password)
+            .ClickLogInButton()
+            .CheckLogInResult(result);
+        }
+        [TestCase("gabriele.rutkauske@gmail.com", "KodasTestui", "Lojalumo taškų istorija", TestName = "Test Loyalty button")]
+        public void TestLoyaltybutton(string firstInput, string secondInput, string result)
+        {
+            _page.InsertEmailAndPassword(firstInput, secondInput)
+            .ClickLogInButton()
+            .LoyaltyButtonClick()
+            .CheckLoyaltyButtonResult(result);
+        }
+    }
+}
